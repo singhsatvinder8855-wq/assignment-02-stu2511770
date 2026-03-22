d7fc56107db89aa3b17bad54476bb7ea9c5b6ddd
singhsatvinder8855-wq <singhsatvinder8855@gmail.com>
2026-03-23 01:08:43 +0530

    Update star_schema.sql

--- part3-datawarehouse/star_schema.sql
+++ part3-datawarehouse/star_schema.sql
@@ -1,1 +1,37 @@
+-- Dimension Table: Date
+CREATE TABLE dim_date (
+    date_id INT PRIMARY KEY,         -- e.g., 20231015
+    full_date DATE NOT NULL,
+    year INT NOT NULL,
+    month INT NOT NULL,
+    month_name VARCHAR(20) NOT NULL,
+    quarter INT NOT NULL
+);
 
+-- Dimension Table: Store
+CREATE TABLE dim_store (
+    store_id VARCHAR(10) PRIMARY KEY,
+    store_name VARCHAR(100) NOT NULL,
+    location_city VARCHAR(50) NOT NULL
+);
+
+-- Dimension Table: Product
+CREATE TABLE dim_product (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL      -- Needs to be cleaned (standard casing)
+);
+
+-- Central Fact Table: Sales
+CREATE TABLE fact_sales (
+    transaction_id VARCHAR(20) PRIMARY KEY,
+    date_id INT NOT NULL,
+    store_id VARCHAR(10) NOT NULL,
+    product_id VARCHAR(10) NOT NULL,
+    quantity_sold INT NOT NULL,
+    unit_price DECIMAL(10, 2) NOT NULL,
+    total_revenue DECIMAL(12, 2) NOT NULL, -- quantity * price
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (store_id) REFERENCES dim_store(store_id),
+    FOREIGN KEY (product_id) REFERENCES dim_product(product_id)
+);
